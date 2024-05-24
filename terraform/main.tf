fb14282c16102729617496071de488b816f234be
sanket363@gmail.com <sanketbhalke363@gmail.com>
2024-05-24 21:32:51 +0530

    udpated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 # provider block defines the cloud provider and its configuration
 provider "aws" {
-  region = "us-east-1"
-  profile = "dfault"
+  region  = "us-east-1"
+  profile = "default"
 }
 
 # variable block allows you to define variables for reusability
@@ -19,18 +19,6 @@ resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
   # other VPC configurations...
 }
-#resource "aws_security_group" "my_security_group" {
-#  vpc_id = aws_vpc.my_vpc.id
-  # other security group configurations...
-#}
-
-resource "aws_instance" "my_instance" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  subnet_id     = aws_subnet.my_subnet.id
-  #security_group = [aws_security_group.my_security_group.id]
-  # other instance configurations...
-}
 
 resource "aws_subnet" "my_subnet" {
   vpc_id                  = aws_vpc.my_vpc.id
@@ -40,9 +28,25 @@ resource "aws_subnet" "my_subnet" {
   # other subnet configurations...
 }
 
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+}
+
+resource "aws_route_table_association" "my_route_table_association" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.my_route_table.id
+}
+
 resource "aws_security_group" "my_sg" {
+  vpc_id      = aws_vpc.my_vpc.id # Ensure the security group is created in the same VPC
   name        = "my-security-group"
   description = "Allow inbound SSH and HTTP traffic"
+
   ingress {
     from_port   = 22
     to_port     = 22
@@ -61,6 +65,16 @@ resource "aws_security_group" "my_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_instance" "my_instance" {
+  ami               = var.ami
+  instance_type     = var.instance_type
+  subnet_id         = aws_subnet.my_subnet.id
+  vpc_security_group_ids = [aws_security_group.my_sg.id] # Correctly reference the security group ID
+  key_name          = "new-test"                    # Specify the key pair name without .pem extension
+
+  # other instance configurations...
 }
 
 # output block allows you to define values to be displayed after apply
